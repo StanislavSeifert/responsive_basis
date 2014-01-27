1ef721a356e2bd32510b5e7a64b5c8e9ca8096f5
Stan <seifert.stanislav@googlemail.com>
2014-01-27 23:39:22 +0100

    remove node modules

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -2,8 +2,9 @@ module.exports = function(grunt) {
 
   grunt: grunt.initConfig({
 
+    // Compass Framework Interpreter
     compass: {
-      dist: {   
+      dist: {
         options: {
           outputStyle: 'expanded', // expanded, compressed, compact, nested
           sassDir: 'css',
@@ -13,22 +14,53 @@ module.exports = function(grunt) {
       }
     },
 
+    // Javascript Dateien Zusammenführen in eine Datei
+    concat: {
+      dist: {
+        src: [
+          'js/libs/*.js', // All JS in the libs folder
+          'js/specific.js'  // This specific file
+        ],
+        dest: '../build/js/application.js'
+      }
+    },
+
+    // Zusammengeführte Jvascript-Datei minimizen
+    uglify: {
+      build: {
+        src: '../build/js/application.js',
+        dest: '../build/js/application.min.js'
+      }
+    },
+
+    // Listener auf veränderte SASS uns JS Dateien
     watch: {
+      // Wenn sich bei SCSS etwas verändert
       sass: {
         files: ['css/sass/**/*.scss', '!css/sass/vendor/**/*.scss'],
         tasks: ['compass:dist'],
         options: {
           livereload: true // Automatisches neuladen im Browsers
         },
-      }
+      },
+      // Wenn sich bei JS etwas verändert
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['concat', 'uglify'], //Zusammenführen & Minimizen
+        options: {
+            spawn: false,
+        }
+      },
     }
 
   });
 
   // Plugin´s
   grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');     // CSS Task
+  grunt.loadNpmTasks('grunt-contrib-compass');  // CSS Task
+  grunt.loadNpmTasks('grunt-contrib-concat');   // JS Task
+  grunt.loadNpmTasks('grunt-contrib-uglify');   // JS Task
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Tasks
---
+++ grunt/setup.sh
@@ -0,0 +1,5 @@
+brew install python
+sudo gem install bundler
+bundle
+sudo npm install -g grunt-cli
+npm install
